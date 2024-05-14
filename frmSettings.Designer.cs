7f643c9e1ee1cd30048bf55da87daf4f151b99da
Gio Andrew Briones <gioandrewbriones@gmail.com>
2024-05-15 00:13:03 +0800

    + added the settings form

---
+++ frmSettings.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace InfoConnect
+{
+    partial class frmSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSettings));
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.btnNotification = new Guna.UI2.WinForms.Guna2ImageRadioButton();
+            this.guna2ImageRadioButton2 = new Guna.UI2.WinForms.Guna2ImageRadioButton();
+            this.guna2ImageRadioButton3 = new Guna.UI2.WinForms.Guna2ImageRadioButton();
+            this.guna2ImageRadioButton4 = new Guna.UI2.WinForms.Guna2ImageRadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
+            this.guna2PictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.ShadowDecoration.Parent = this.guna2PictureBox1;
+            this.guna2PictureBox1.Size = new System.Drawing.Size(904, 459);
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // btnNotification
+            // 
+            this.btnNotification.BackColor = System.Drawing.Color.Transparent;
+            this.btnNotification.CheckedState.Parent = this.btnNotification;
+            this.btnNotification.HoverState.Image = ((System.Drawing.Image)(resources.GetObject("btnNotification.HoverState.Image")));
+            this.btnNotification.HoverState.ImageSize = new System.Drawing.Size(182, 38);
+            this.btnNotification.HoverState.Parent = this.btnNotification;
+            this.btnNotification.Image = ((System.Drawing.Image)(resources.GetObject("btnNotification.Image")));
+            this.btnNotification.ImageSize = new System.Drawing.Size(89, 10);
+            this.btnNotification.Location = new System.Drawing.Point(0, 139);
+            this.btnNotification.Name = "btnNotification";
+            this.btnNotification.PressedState.Parent = this.btnNotification;
+            this.btnNotification.Size = new System.Drawing.Size(204, 39);
+            this.btnNotification.TabIndex = 3;
+            this.btnNotification.UseTransparentBackground = true;
+            // 
+            // guna2ImageRadioButton2
+            // 
+            this.guna2ImageRadioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ImageRadioButton2.CheckedState.Parent = this.guna2ImageRadioButton2;
+            this.guna2ImageRadioButton2.HoverState.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageRadioButton2.HoverState.Image")));
+            this.guna2ImageRadioButton2.HoverState.ImageSize = new System.Drawing.Size(182, 38);
+            this.guna2ImageRadioButton2.HoverState.Parent = this.guna2ImageRadioButton2;
+            this.guna2ImageRadioButton2.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageRadioButton2.Image")));
+            this.guna2ImageRadioButton2.ImageSize = new System.Drawing.Size(150, 13);
+            this.guna2ImageRadioButton2.Location = new System.Drawing.Point(0, 184);
+            this.guna2ImageRadioButton2.Name = "guna2ImageRadioButton2";
+            this.guna2ImageRadioButton2.PressedState.Parent = this.guna2ImageRadioButton2;
+            this.guna2ImageRadioButton2.Size = new System.Drawing.Size(204, 50);
+            this.guna2ImageRadioButton2.TabIndex = 2;
+            this.guna2ImageRadioButton2.UseTransparentBackground = true;
+            // 
+            // guna2ImageRadioButton3
+            // 
+            this.guna2ImageRadioButton3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ImageRadioButton3.CheckedState.Parent = this.guna2ImageRadioButton3;
+            this.guna2ImageRadioButton3.HoverState.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageRadioButton3.HoverState.Image")));
+            this.guna2ImageRadioButton3.HoverState.ImageSize = new System.Drawing.Size(182, 38);
+            this.guna2ImageRadioButton3.HoverState.Parent = this.guna2ImageRadioButton3;
+            this.guna2ImageRadioButton3.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageRadioButton3.Image")));
+            this.guna2ImageRadioButton3.ImageSize = new System.Drawing.Size(119, 13);
+            this.guna2ImageRadioButton3.Location = new System.Drawing.Point(0, 240);
+            this.guna2ImageRadioButton3.Name = "guna2ImageRadioButton3";
+            this.guna2ImageRadioButton3.PressedState.Parent = this.guna2ImageRadioButton3;
+            this.guna2ImageRadioButton3.Size = new System.Drawing.Size(204, 44);
+            this.guna2ImageRadioButton3.TabIndex = 3;
+            this.guna2ImageRadioButton3.UseTransparentBackground = true;
+            // 
+            // guna2ImageRadioButton4
+            // 
+            this.guna2ImageRadioButton4.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ImageRadioButton4.CheckedState.Parent = this.guna2ImageRadioButton4;
+            this.guna2ImageRadioButton4.HoverState.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageRadioButton4.HoverState.Image")));
+            this.guna2ImageRadioButton4.HoverState.ImageSize = new System.Drawing.Size(182, 38);
+            this.guna2ImageRadioButton4.HoverState.Parent = this.guna2ImageRadioButton4;
+            this.guna2ImageRadioButton4.Image = ((System.Drawing.Image)(resources.GetObject("guna2ImageRadioButton4.Image")));
+            this.guna2ImageRadioButton4.ImageSize = new System.Drawing.Size(59, 10);
+            this.guna2ImageRadioButton4.Location = new System.Drawing.Point(0, 290);
+            this.guna2ImageRadioButton4.Name = "guna2ImageRadioButton4";
+            this.guna2ImageRadioButton4.PressedState.Parent = this.guna2ImageRadioButton4;
+            this.guna2ImageRadioButton4.Size = new System.Drawing.Size(204, 45);
+            this.guna2ImageRadioButton4.TabIndex = 4;
+            this.guna2ImageRadioButton4.UseTransparentBackground = true;
+            // 
+            // frmSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 459);
+            this.Controls.Add(this.guna2ImageRadioButton4);
+            this.Controls.Add(this.guna2ImageRadioButton3);
+            this.Controls.Add(this.guna2ImageRadioButton2);
+            this.Controls.Add(this.btnNotification);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.Name = "frmSettings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmSettings";
+            this.Load += new System.EventHandler(this.frmSettings_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2ImageRadioButton btnNotification;
+        private Guna.UI2.WinForms.Guna2ImageRadioButton guna2ImageRadioButton2;
+        private Guna.UI2.WinForms.Guna2ImageRadioButton guna2ImageRadioButton3;
+        private Guna.UI2.WinForms.Guna2ImageRadioButton guna2ImageRadioButton4;
+    }
+}
---
+++ frmSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfoConnect
+{
+    public partial class frmSettings : Form
+    {
+        public frmSettings()
+        {
+            InitializeComponent();
+        }
+
+        // disables the frmMain while the frmSettings is loaded (BUG: frmMain doesn't enable when the frmSettings is closed)
+        private void frmSettings_Load(object sender, EventArgs e)
+        {
+            frmMain.ActiveForm.Enabled = false;
+        }
+
+
+    }
+}
